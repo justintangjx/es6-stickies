9cde0569916e69e4a496186f86828b22c25a42f5
“justintangjx” <just.think.tang@gmail.com>
2018-03-11 20:09:58 +0800

    arrow functions for autobinding

--- scripts/main.es6.js
+++ scripts/main.es6.js
@@ -48,10 +48,10 @@ this.addNoteButton = document.getElementById("save");
 this.notesSectionTitle = document.getElementById("notes-section-title");
 
 // Saves notes on button click.
-this.addNoteButton.addEventListener("click", this.saveNote.bind(this));
+this.addNoteButton.addEventListener("click", () => this.saveNote());
 
 // Toggle for the button.
-this.noteMessageInput.addEventListener("keyup", this.toggleButton.bind(this));
+this.noteMessageInput.addEventListener("keyup", () => this.toggleButton());
 
 // Loads all the notes.
 for (var key in localStorage) {
@@ -59,17 +59,12 @@ for (var key in localStorage) {
 }
 
 // Listen for updates to notes from other windows.
-window.addEventListener(
-  "storage",
-  function(e) {
-    this.displayNote(e.key, e.newValue);
-  }.bind(this)
-);
+window.addEventListener("storage", e => {
+  this.displayNote(e.key, e.newValue);
+});
 
 // On load start the app.
-window.addEventListener("load", function() {
-  new StickyNotesApp();
-});
+window.addEventListener("load", () => new StickyNotesApp());
 
 // A Sticky Note custom element that extends HTMLElement.
 var StickyNote = Object.create(HTMLElement.prototype);
